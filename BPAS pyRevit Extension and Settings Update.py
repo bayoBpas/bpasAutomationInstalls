0cb661196624496934adca7fa0fbf61ed8e021c9
Bayonle Windapo <bayo@bpas.co.za>
2023-01-03 23:50:16 +0200

    Modifications to the pyrevit, dynamo packages, and dynamo player scripts.

--- BPAS pyRevit Extension and Settings Update.py
+++ BPAS pyRevit Extension and Settings Update.py
@@ -1,51 +1,100 @@
-#import the required libraries
+# Import the required libraries
 import os
 import shutil
 import time
 import configparser
 import sys
+import psutil
 
-#check if the source directory of the BPAS pyRevit extension is accessible. Quit if not
-if not os.path.exists(r'Z:\CAD Projects\B - BIM TEMPLATES\I - PYREVIT\bpas'):quit()
+# Function for checking if the required paths exist.
+def paths_exist(*paths):
+    """
+    Check if any of the input paths are accessible.
+    Quit if any of the paths are not accessible.
+    Requires the os library to run.
+    """
+    for path in paths:
+        if not os.path.exists(path):
+            sys.exit("One or more of the paths does not exist")
+
+# Path to be checked.
+pyrevit_extension_bpas = r'Z:\CAD Projects\B - BIM TEMPLATES\I - PYREVIT\bpas'
+
+# Check if the paths exist.
+try:
+    paths_exist(pyrevit_extension_bpas)
+    print("The paths exist")
+except:
+    print("The paths do not exist")
+    quit()
+
+
+# Function for checking if revit is running. Quit if running.
+def programs_running(*programs):
+    """
+    Check if any of the input programs are running in the task manager.
+    Quit if any of the input programs is found running.
+    Requires the sys and psutil libraries to run.
+    """
+    running_processes = list(p.name() for p in psutil.process_iter())
+
+    # Check if any of the input programs are in 'running_processes'.
+    for program in programs:
+        if program in running_processes:
+            sys.exit("One or more of the programs was found running")
+
+# Check if Revit is running.
+try:
+    programs_running('revit.exe', 'Revit.exe')
+    print("Revit is not running")
+except:
+    print("Revit is running")
+    quit()
+
 
 ###############download BPAS extension file to computer###############
 
-#get current username in session
+# Get current username in session
 userName = os.getlogin()
 print(userName)
 
-#target directory for the bpas extensions folder. Combine username with directory path beginning and end
+# Target directory for the bpas extensions folder.
+# Combine username with directory path beginning and end.
 begDir = r'C:/Users/'
 endDir = 'AppData/Roaming/pyRevit/bpas'
 target_dir = os.path.join(begDir,userName,endDir)
 print(target_dir)
 
 
-# Source directory for the bpas pyRevit extension folder on the server
+# Source directory for the bpas pyRevit extension folder on the server.
 source_dir = r'Z:\CAD Projects\B - BIM TEMPLATES\I - PYREVIT\bpas'
 
 
-#get all file names in the source directory
+# Get all file names in the source directory.
 fileNames = os.listdir(source_dir)
 print(fileNames)
 
 
-# Check if the target directory exists
+# Check if the target directory exists.
 if not os.path.exists(target_dir):
-    # Copy the bpas extension folder if the folder does not exist at the target path
+    # Copy the bpas extension folder if the folder does not exist at the
+    # target path.
     shutil.copytree(source_dir, target_dir)
 
 
-#check if the source directory has been modified more recently than the target directory
-print(os.path.exists(source_dir) and os.path.getmtime(source_dir) > os.path.getmtime(target_dir))
-if os.path.exists(source_dir) and os.path.getmtime(source_dir) > os.path.getmtime(target_dir):
+# Check if the source directory has been modified more recently than the
+# target directory.
+print(os.path.exists(source_dir) and os.path.getmtime(source_dir) > \
+    os.path.getmtime(target_dir))
+if os.path.exists(source_dir) and os.path.getmtime(source_dir) > \
+    os.path.getmtime(target_dir):
 
-    #delete target directory if it exists
+    # Delete target directory if it exists.
     if os.path.exists(target_dir):
-        # Delete file
+        # Delete file.
         if os.path.isfile(target_dir):
             os.remove(target_dir)
-        # Delete directory
+        # Delete directory.
         else:
             shutil.rmtree(target_dir)
 
@@ -56,29 +105,32 @@ else:
 
 #################pyRevit settings and config file update##################
 
-#open the pyRevit configuration files from the current users computer
+# Open the pyRevit configuration files from the current users computer.
 conBegDir = r'C:/Users/'
 conEndDir = 'AppData\Roaming\pyRevit\pyRevit_config.ini'
 configPath = os.path.join(conBegDir,userName,conEndDir)
 
-#check if the bpas extension file has been updated more recently than the config file
-print(os.path.exists(target_dir) and os.path.getmtime(target_dir) > os.path.getmtime(configPath))
+# Check if the bpas extension file has been updated more recently than the
+# config file.
+print(os.path.exists(target_dir) and os.path.getmtime(target_dir) > \
+    os.path.getmtime(configPath))
 if os.path.getmtime(target_dir) > os.path.getmtime(configPath):
 
-    #modify the config file if the bpas extension file has been modified more recently than the config file
+    # Modify the config file if the bpas extension file has been modified more
+    # recently than the config file.
 
-    #read the current contents of the config file
+    # Read the current contents of the config file.
     config = configparser.ConfigParser()
     config.read(configPath)
 
-    #create text to be written to config files
+    # Create text to be written to config files
     configText = '["'+target_dir+'"]'
     print(configText)
 
-    #get the contents of the pyRevit config file at the core in userextensions
+    # Get the contents of the pyRevit config file at the core in userextensions.
     print((config.get("core", "userextensions")))
 
-    #write the new contents to the config file
+    # Write the new contents to the config file.
     config.set("core", "userextensions", configText)
     with open(configPath, "w") as config_file:
         config.write(config_file)
--- Dynamo Player Settings File Installation.py
+++ Dynamo Player Settings File Installation.py
@@ -1,47 +1,98 @@
-#import the required libraries
+# Import the required libraries.
 import os
 import shutil
 import time
+import psutil
 
-#check if the source directory is accessible. Quit if not
-if not os.path.exists(r'Z:`\IT\Dynamo Player Settings\dynamoplayer-5'):quit()
 
-#get current username in session
+# Function for checking if the required paths exist.
+def paths_exist(*paths):
+    """
+    Check if any of the input paths are accessible.
+    Quit if any of the paths are not accessible.
+    Requires the os library to run.
+    """
+    for path in paths:
+        if not os.path.exists(path):
+            sys.exit("One or more of the paths does not exist")
+
+# Path to be checked.
+dynamo_settings_path = r'Z:\IT\Dynamo Player Settings\dynamoplayer-5'
+
+# Check if the paths exist.
+try:
+    paths_exist(dynamo_settings_path)
+    print("The paths exist")
+except:
+    print("The paths do not exist")
+    quit()
+
+
+# Function for checking if revit is running. Quit if running.
+def programs_running(*programs):
+    """
+    Check if any of the input programs are running in the task manager.
+    Quit if any of the input programs is found running.
+    Requires the sys and psutil libraries to run.
+    """
+    running_processes = list(p.name() for p in psutil.process_iter())
+
+    # Check if any of the input programs are in 'running_processes'.
+    for program in programs:
+        if program in running_processes:
+            sys.exit("One or more of the programs was found running")
+
+# Check if Revit is running.
+try:
+    programs_running('revit.exe', 'Revit.exe')
+    print("Revit is not running")
+except:
+    print("Revit is running")
+    quit()
+
+
+
+# Get current username in session.
 userName = os.getlogin()
 print(userName)
 
-# Target directory for the dynamo player settings.Combine username with directory path beginning and end
+# Target directory for the dynamo player settings.
+#Combine username with directory path beginning and end.
 begDir = r'C:/Users/'
 endDir = 'AppData\Local\dynamoplayer-5'
 target_dir = os.path.join(begDir,userName,endDir)
 print(target_dir)
 
 
-# Source directory for the dynamo player settings on the server
+# Source directory for the dynamo player settings on the server.
 source_dir = r'Z:\IT\Dynamo Player Settings\dynamoplayer-5'
 
 
-#get all names files in the source directory
+# Get all names files in the source directory.
 fileNames = os.listdir(source_dir)
 print(fileNames)
 
 
-# Check if the target directory exists
+# Check if the target directory exists.
 if not os.path.exists(target_dir):
-    # Copy the dynamo player settings folder if the folder does not exist at the target path
+    # Copy the dynamo player settings folder if the folder does not exist at 
+    # the target path.
     shutil.copytree(source_dir, target_dir)
 
 
-#check if the source directory has been modified more recently than the target directory
-print(os.path.exists(source_dir) and os.path.getmtime(source_dir) > os.path.getmtime(target_dir))
-if os.path.exists(source_dir) and os.path.getmtime(source_dir) > os.path.getmtime(target_dir):
+# Check if the source directory has been modified more recently.
+# than the target directory.
+print(os.path.exists(source_dir) and os.path.getmtime(source_dir) > \
+    os.path.getmtime(target_dir))
+if os.path.exists(source_dir) and os.path.getmtime(source_dir) > \
+    os.path.getmtime(target_dir):
 
-    #delete target directory if it exists
+    # Delete target directory if it exists.
     if os.path.exists(target_dir):
-        # Delete file
+        # Delete file.
         if os.path.isfile(target_dir):
             os.remove(target_dir)
-        # Delete directory
+        # Delete directory.
         else:
             shutil.rmtree(target_dir)
 
